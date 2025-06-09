6466ce9251d695e03602514a10ca3c7bc4c3b0be
Gui-Test <160752405+Gui-Test@users.noreply.github.com>
2025-06-09 15:10:52 -0300

    Update vetorExc1.c

--- atividadesC/vetorExc1.c
+++ atividadesC/vetorExc1.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
-#define N 10
+//Exc1
+/*#define N 8
+
+int main(){
+    int i,n;
+    int v[N]={1, 3, 4 ,5, 6, 7, 8, 9};
+    
+    printf("\nDigite um Número:\n");
+    scanf("%i", &n);
+    printf("\nSomado ao Vetor é igual a:\n");
+
+    printf("\n{");
+    for (i=0;i<N;i++){
+        v[i] = v[i] + n;
+        printf("%i,",v[i]);
+    }
+    printf("}\n");
+}
+*/
+
+//Exc2
+/*#define N 10
 
 int main(){
     int i;
-    int v[10]={1, 3, 4 ,5, 6, 7, 8, 9};
-    
+    int A[N]={1,3,4,5,6,7,8,9,-1,2};
+    int B[N]={14,10,6,-5,-2,7,12,99,0,2},C[10];
 
     printf("{");
-    for (i=0;i<10;i++){
+    for (i=0;i<N;i++){
         C[i] = A[i] + B[i];
         printf("%i,",C[i]);
     }
     printf("}");
-}
+}*/
+
+//Exc3
