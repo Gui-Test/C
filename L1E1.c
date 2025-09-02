238079c1f7d3f5a00046c7276ce24c45a4db204a
Guilherme S Hille - Batata773 <160752405+Gui-Test@users.noreply.github.com>
2025-09-02 15:52:20 -0300

    Exc 1 lista 1

---
+++ L1E1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float x,y;
+    printf("Digite as coordenadas x e y:\n");
+    scanf("%f %f", &x,&y);
+
+    if (x > 0) {
+        if (y > 0){
+            printf("Quadrante 1");
+        }else{
+            printf("Quadrante 4");
+        }
+    }else if (x<0){
+        if (y > 0){
+            printf("Quadrante 2");
+        }else {
+            printf("Quadrante 3");
+        }
+    }else if (x==0){
+        printf("Origem");
+    }
+
+    return 0;
+}
