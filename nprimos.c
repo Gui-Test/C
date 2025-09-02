954de7bb562c81534055679022ca1290c89d16c7
Guilherme <guilhermeshille@gmail.com>
2025-09-02 11:16:37 -0300

    consertei o nprimos

--- nprimos.c
+++ nprimos.c
@@ -6,34 +6,28 @@ int ehprimo(int k);
 
 int main(int argc, char const *argv[]){
     int n,k, i = 0;
-    printf("Escreva quantos primos você quer");
+    printf("Escreva quantos primos você quer \n");
     scanf("%d", &n);
-    printf("Escreva a partir de onde quer pegar.");
+    printf("Escreva a partir de onde quer pegar. \n");
     scanf("%d", &k);
 
     while (i < n){
         if (ehprimo(k)){
-            printf("%d \n", &k);
+            printf("%d \n", k);
             i++;
-        }else{
-         k++;
         }
+         k++;
     }
 
 }
 
 int ehprimo(int k){
-    int n2, m,  eh_primo=0;
-    for(n2 = k; eh_primo; n2++){
-        int i; //Verdadeiro
-        float r = sqrt(k);
-        for(i = 2; i <= r && eh_primo; i++){
-            if(n2 % i == 0){
-                eh_primo = 0; //Falso
-            }else{
-                eh_primo = 1;
+    int j;
+    float r = (sqrt(k));
+        for (j = 2;j <= r;j++){
+            if (k % j == 0){
+                return 0;
             }
         }
-        return eh_primo;
-    }
+        return 1;
 }
