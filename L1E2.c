3444da447dc510f9fac7d5df6b773acabcbc50dc
Guilherme S Hille - Batata773 <160752405+Gui-Test@users.noreply.github.com>
2025-09-04 16:56:06 -0300

    Update L1E2.c

--- L1E2.c
+++ L1E2.c
@@ -1,31 +1,32 @@
-#include <stdio.h>
-
 //Triângulo
+#define N 3
 
-float ordem(float x,float y, float z);
+double maior(double x,double y, double z);
 
 int main(int argc, char const *argv[]){
-    float a,b,c, aux;
+    double a,b,c, aux;
 
     printf("Digite os lados do triangulo a b c:\n");
-    scanf("%f %f %f", &a,&b,&c);
-
-    ordem(a,b,c);
-    printf("%f %f %f", a, b, c);
-
-
-}
-
-float ordem(float x,float y, float z){
-    float maior = x;
-    if (y > maior){
-        maior = y;
-        y = x;
+    scanf("%lf %lf %lf", &a,&b,&c);
+    
+    //selection_sort: não é tão bom mas eu tinha esquecido como fazer
+    double v[] = {a,b,c};
+    int i = 0;
+    for (i; i<N-1; i++){
+        int i_maior = i, j;
+        for (j = 1; j < N; j++){
+            if(v[j] > v[i_maior]){
+                i_maior = j;
+            }
+        }
+        aux = v[i];
+        v[i] = v[i_maior];
+        v[i_maior] = aux;
     }
-    if (z > maior){
-        maior = z;
-        z = y;
+    a = v[0]; b = v[1]; c = v[2];
+    printf("%lf %lf %lf \n", a, b, c);
+
+    if (a >= b+c){
+        
     }
-    x = maior;
-    return x,y,z;
 }
