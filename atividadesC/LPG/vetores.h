a434396c0a8c982d6d64858505817eee9c232294
Guilherme <guilhermeshille@gmail.com>
2025-09-18 11:17:40 -0300

    Pasta pra busca binaria

--- atividadesC/LPG/vetores.h
+++
@@ -1,7 +0,0 @@
-
-//buscando as funções do vetores.c
-void gera_valores(int v[], int n);
-void gera_valores_ordem(int v[], int n,int limite);
-int busca_binaria (int v[], int n, int chave);
-void mostra_vetor(int v[], int n);
-
