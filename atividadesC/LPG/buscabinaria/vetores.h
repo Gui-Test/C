13e0916019aab38febf56680f89559497fe5ce56
Guilherme S Hille - Batata773 <160752405+Gui-Test@users.noreply.github.com>
2025-09-23 16:52:42 -0300

    Update vetores.h

--- atividadesC/LPG/buscabinaria/vetores.h
+++ atividadesC/LPG/buscabinaria/vetores.h
@@ -1,7 +1,13 @@
 
 //buscando as funções do vetores.c
-void gera_valores(int v[], int n);
+void gera_valores(int v[], int n,int limite);
 void gera_valores_ordem(int v[], int n,int limite);
 int busca_binaria (int v[], int n, int chave);
 void mostra_vetor(int v[], int n);
+
+//funções com matriz 
+void mostra_matriz(int lin, int col,int x[lin][col]);
+float media_pares(int l, int c,int x[l][c]);
+void gera_transposta(int l, int c,int x[l][c], int t[c][l]);
+void soma_matriz(int l, int c,int x[l][c], int y[l][c],int t[l][c]);
 
