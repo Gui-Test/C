1fc33a2254fb83af2687387748994a80d865e5d8
Guilherme <guilhermeshille@gmail.com>
2025-10-14 19:00:09 -0300

    Lista5 Exc10 cocluido

--- atividadesC/LPG/Lista5/L5E10.c
+++ atividadesC/LPG/Lista5/L5E10.c
@@ -2,3 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 //Exc10 Inverte String
+
+#define TAM 20
+void reverse(char s[]);
+
+int main(){
+    char str[TAM];
+    scanf("%s", str);
+    printf("String: %s \n", str);
+    reverse(str);
+    printf("String com reverse: %s \n", str);
+
+}
+
+void reverse(char s[]){
+    int i,j;
+    char aux = ' ';
+    for(i=0;s[i]!='\0';i++);
+    int size=i;
+    i--;
+    for(j=0,i=i;j<(size/2);j++,i--){
+        aux  = s[j];
+        s[j] = s[i];
+        s[i] = aux;
+    }
+}
