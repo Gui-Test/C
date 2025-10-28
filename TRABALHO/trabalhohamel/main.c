668e51b472edd8ecc9683c8b14f0a24225e6e42f
Guilherme <guilhermeshille@gmail.com>
2025-10-28 16:21:09 -0300

    UNIAO COMPLETA, FUSÃOOOOOO

---
+++ TRABALHO/trabalhohamel/main.c
@@ -0,0 +1,9 @@
+#include "header.h"
+//gcc main.c header.h menu.c conjuntos.c
+
+int main(){
+    menu();
+
+
+    return 0;   
+}
---
+++ TRABALHO/trabalhohamel/menu.c
@@ -0,0 +1,62 @@
+#include "header.h"
+
+void menu(){
+    int conjuntos[M][N], cont=0, opcao=0;
+    gera_matriz(conjuntos, cont);
+    do {
+        printf("Gerenciamento de Conjuntos\n\nMenu\n\n");
+        printf("1-Criar um novo conjunto vazio.\n");
+        printf("2-Inserir dados em um conjunto.\n");
+        printf("3-Remover um conjunto.\n");
+        printf("4-Fazer uniao entre dois conjuntos.\n");
+        printf("5-Fazer a interseccao entre dois conjuntos.\n");
+        printf("6-Mostrar um conjunto.\n");
+        printf("7-Mostrar todos os conjuntos.\n");
+        printf("8-Fazer busca por um valor.\n");
+        printf("9-Sair do Programa\n\n");
+
+        printf("Escolha uma opcao: ");
+        scanf("%i", &opcao);
+        printf("\n");
+
+        switch (opcao) {
+            case 1:
+                cria_conj(conjuntos, &cont);
+                pausa();
+                break;
+            case 2:
+                insere_dados(conjuntos, cont);
+                pausa();
+                break;
+            case 3:
+                remover(conjuntos, &cont);
+                pausa();
+                break;
+            case 4:
+                uniao(conjuntos, &cont);
+                pausa();
+                break;
+            case 5:
+                inter(conjuntos, &cont);
+                pausa();
+                break;
+            case 6:
+                mostra_conj(conjuntos, cont);
+                pausa();
+                break;
+            case 7:
+                mostra_todos(conjuntos, cont);
+                pausa();
+                break;
+            case 8:
+                buscar(conjuntos, cont);
+                pausa();
+                break;
+            case 9:
+                printf("Encerrando o programa.\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+            }
+    } while(opcao!=9);
+}
