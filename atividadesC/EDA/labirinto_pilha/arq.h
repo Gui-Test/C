f1e6c871917534197817e5bc1a22dc0388b32766
Guilherme <guilhermeshille@gmail.com>
2026-04-01 10:36:10 -0300

    começo do labirinto

---
+++ atividadesC/EDA/labirinto_pilha/arq.h
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#define SIM 1
+#define NAO 0
+#define SUCESSO 1
+#define FRACASSO 0
+
+typedef struct {
+    
+}info;
+
+
+struct caixa{	
+	info dados;
+	struct caixa *abaixo;
+};	
+
+struct pilha{ 
+	struct caixa *topo;
+	int tamPilha;
+};
